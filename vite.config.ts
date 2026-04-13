981e9a575154cf4a49233ac8242082b868a70d55
opipowe <zadorozniykirill066@gmail.com>
2026-04-13 22:48:13 +0300

    Fix: restore design by removing postcss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
     },
-  },
-  server: {
-    port: 3000,
-  },
+  };
 });
